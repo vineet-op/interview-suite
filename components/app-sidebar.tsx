8fce7043081e0e6d37ba7d5085391d032138ba70
Vineet Jadhav <vineet290403@gmail.com>
2025-05-04 15:28:08 +0530

    Updated Layout.tsx

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,4 +1,4 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { Calendar, Home, Inbox } from "lucide-react"
 
 import {
     Sidebar,
@@ -32,26 +32,28 @@ const items = [
 
 export function AppSidebar() {
     return (
-        <Sidebar>
-            <SidebarContent>
-                <SidebarGroup >
-                    <SidebarGroupLabel className="font-bold text-blue-400 text-base">Prep-Suite</SidebarGroupLabel>
-                    <SidebarGroupContent className="mt-2">
-                        <SidebarMenu className="flex flex-col gap-5">
-                            {items.map((item) => (
-                                <SidebarMenuItem key={item.title}>
-                                    <SidebarMenuButton asChild>
-                                        <a href={item.url}>
-                                            <item.icon />
-                                            <span>{item.title}</span>
-                                        </a>
-                                    </SidebarMenuButton>
-                                </SidebarMenuItem>
-                            ))}
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
-            </SidebarContent>
-        </Sidebar>
+        <>
+            <Sidebar>
+                <SidebarContent>
+                    <SidebarGroup >
+                        <SidebarGroupLabel className="font-bold text-blue-400 text-base">Prep-Suite</SidebarGroupLabel>
+                        <SidebarGroupContent className="mt-2">
+                            <SidebarMenu className="flex flex-col gap-5">
+                                {items.map((item) => (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild>
+                                            <a href={item.url}>
+                                                <item.icon />
+                                                <span>{item.title}</span>
+                                            </a>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                ))}
+                            </SidebarMenu>
+                        </SidebarGroupContent>
+                    </SidebarGroup>
+                </SidebarContent>
+            </Sidebar>
+        </>
     )
 }
