f30550cf1e3118266897ee451e029562d72b3171
Vineet Jadhav <vineet290403@gmail.com>
2025-05-05 23:42:08 +0530

    Added previous interview-info

--- app/(dashbaord)/interview/page.tsx
+++ app/(dashbaord)/interview/page.tsx
@@ -1,17 +1,36 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { Plus } from 'lucide-react'
 import Modal from '@/components/Modal/page'
+import axios from 'axios'
+
+interface InterviewInfo {
+    role: string
+    jobDescription: string
+    createdAt: string
+}
 
 const page = () => {
 
+
     const [open, setOpen] = useState<boolean>(false)
+    const [interviewInfo, setInterviewInfo] = useState<InterviewInfo[]>([])
+
+    async function getInterviewInfo() {
+        const response = await axios.get("http://localhost:3000/api/get-interview-info")
+        setInterviewInfo(response.data.interview)
+    }
+
+    useEffect(() => {
+        getInterviewInfo()
+    }, [])
+
 
     return (
-        <div className="w-screen h-screen pt-16 pl-10">
-            <div className="text-blue-500 font-semibold text-xl pb-16">
+        <div className="w-screen h-screen pt-16 ">
+            <div className="text-blue-500 font-semibold text-xl pb-4">
                 Interview Prep
             </div>
             <div className="flex gap-4">
@@ -26,6 +45,28 @@ const page = () => {
             {open && <div>
                 <Modal onClose={() => setOpen(false)} />
             </div>}
+
+            <div className='flex flex-col items-stretch w-full'>
+                <div className='text-2xl font-medium mt-10'>
+                    Previous Interview
+                </div>
+                <div className="grid lg:grid-cols-4 sm:grid-cols-1 gap-2  max-w-8xl mt-5">
+                    {interviewInfo.map((data, index) => (
+                        <div key={index} className="group shadow-md rounded-lg py-3 gap-5 border border-gray-200 hover:shadow-lg transition-shadow w-full h-full px-2  hover:bg-blue-500 cursor-pointer">
+                            <div className="text-lg font-semibold text-blue-600 group-hover:text-white">{data?.role}</div>
+                            <div className="text-gray-600 line-clamp-3 group-hover:text-white">{data?.jobDescription}</div>
+                            <div className="text-gray-600 line-clamp-3 group-hover:text-white">
+                                {new Date(data?.createdAt).toLocaleDateString('en-GB', {
+                                    day: '2-digit',
+                                    month: '2-digit',
+                                    year: '2-digit'
+                                })}
+                            </div>
+
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
 
     )
---
+++ app/api/get-interview-info/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server"
+import prisma from "@/lib/prisma"
+
+
+export async function GET(req: Request) {
+
+    const experience = await prisma.interviews.findMany({
+        where: {
+            experience: "1"
+        },
+        select: {
+            role: true,
+            jobDescription: true,
+            createdAt: true
+        }
+    })
+    return NextResponse.json({
+        interview: experience
+    })
+
+}
